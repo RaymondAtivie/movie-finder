6300e1880b2eb2771a6ce0cf1e14d1b326560d03
Raymond Ativie <RaymondAtivie@gmail.com>
2020-08-27 02:51:53 +0100

    cleanup

--- store/movies.ts
+++ store/movies.ts
@@ -57,28 +57,36 @@ export const mutations: MutationTree<NotificationState> = {
   }
 }
 export const actions: ActionTree<NotificationState, RootState> = {
-  async loadPopularMovies({ commit }) {
-    const response = await this.$axios.get("/movie/popular?api_key=9099d4a456925cc52c8aed25ab61ba4e");
+  loadPopularMovies({ commit }) {
+    this.$axios.get("/movie/popular?api_key=9099d4a456925cc52c8aed25ab61ba4e")
+      .then(response => {
+        const movies: Movie[] = Movie.fromArray(response.data.results).slice(0, 16);
 
-    const movies: Movie[] = Movie.fromArray(response.data.results).slice(0, 16);
+        commit('LOAD_POPULAR_MOVIES', movies);
 
-    commit('LOAD_POPULAR_MOVIES', movies);
+      })
+      .catch(error => {
+        this.app.$toast.error("Something went wrong when getting popular moveis", { duration: 5000 });
+      })
+
   },
 
-  async search({ commit }, query: string) {
-    // commit('UPDATE_SLIDE_INDEX', 0);
-
+  search({ commit }, query: string) {
     if (!query) return;
 
-    const response = await this.$axios.get(`/search/movie?api_key=9099d4a456925cc52c8aed25ab61ba4e&query=${query}`);
+    this.$axios.get(`/search/movie?api_key=9099d4a456925cc52c8aed25ab61ba4e&query=${query}`).then(response => {
+      const movies: Movie[] = Movie.fromArray(response.data.results);
 
-    const movies: Movie[] = Movie.fromArray(response.data.results);
+      commit('UPDATE_AUTO_COMPLETE', movies);
+      commit('LOAD_MOVIES', movies);
+    })
+      .catch(error => {
+        this.app.$toast.error("Something went wrong with search", { duration: 5000 });
+      })
 
-    commit('UPDATE_AUTO_COMPLETE', movies);
-    commit('LOAD_MOVIES', movies);
   },
 
-  async getMovie({ state }, movie_id: number): Promise<Movie> {
+  getMovie({ state }, movie_id: number): Promise<Movie> {
     return new Promise((resolve, reject) => {
       const foundMovie = state.allMovies.find(movie => movie.id == movie_id)
       if (foundMovie) {
@@ -88,8 +96,11 @@ export const actions: ActionTree<NotificationState, RootState> = {
       this.$axios.get(`/movie/${movie_id}?api_key=9099d4a456925cc52c8aed25ab61ba4e`)
         .then((response) => {
           resolve(Movie.fromJson(response.data))
-        });
-
+        })
+        .catch(error => {
+          this.app.$toast.error("Oh no. We could not find this movie", { duration: 5000 });
+          reject(error);
+        })
     })
   }
 }
